edcc38504abde71323327601ec697125277ded86
pippoide <giovannipoma28@gmail.com>
2024-04-17 00:09:46 +0200

    UI setup
    Fix Framer Motion error
    Added UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,85 @@
+'use client';
+
+import { motion, useDragControls, useMotionValue, useTransform } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const controls = useDragControls()
+  const x = useMotionValue(0);
+
+  const opacity = useTransform(x, [-100, -50, 0, 50, 100], [0.8, 1, 1, 1, 0.8]);
+  const scale = useTransform(x, [-200, 0, 200], [0.5, 1, 0.5]);
+  const rotate = useTransform(x, [-200, 0, 200], [-45, 0, 45]);
+  const textOpacityLeft = useTransform (x, [0,-50],[0,1]);
+  const textOpacityRight = useTransform (x, [0,50],[0,1])
+
+
+  const handleDragEnd = (event, info) => { 
+
+
+    if (info.offset.x > 1) {
+  
+      // Esegui azione di swipe a destra
+      console.log("Swipe a destra!");
+    } else if (info.offset.x < -1) {
+ 
+      // Esegui azione di swipe a sinistra
+      console.log("Swipe a sinistra!");
+    }
+
+    // Resetta la posizione del div
+    x.set(0);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="sm:w-1/3 w-full min-h-screen bg-red-50 p-5 relative flex flex-col justify-between">  {/**column */}
+        {/**icons */}
+        <div className="w-full flex flex-row justify-evenly items-center">
+          <div className="w-10 h-10 relative">
+            <Image src="/bilancia.svg" fill objectFit="contain" className="point-event-none" draggable="false"></Image>
+          </div>
+          <div className="w-10 h-10 relative">
+            <Image src="/scienza.svg" fill objectFit="contain" className="point-event-none" draggable="false"></Image>
+          </div>
+          <div className="w-10 h-10 relative">
+            <Image src="/persona.svg" fill objectFit="contain" className="point-event-none" draggable="false"></Image>
+          </div>
+        </div>
+        {/**card */}
+        <div className="w-full aspect-square relative">
+          <motion.div
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragControls={controls}
+            onDragEnd={handleDragEnd}
+            className="w-full aspect-square relative z-10"
+            style={{
+              x,
+              opacity,
+              scale,
+              rotate
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div className="w-full aspect-square relative">
+              <motion.h1 className="text-3xl z-10 cursor-pointer-none text-white absolute right-0 p-5 " style={{opacity:textOpacityRight}}>destra</motion.h1>
+              <motion.h1 className="text-3xl z-10 cursor-pointer-none text-white absolute left-0 p-5" style={{opacity:textOpacityLeft}}>sinistra</motion.h1>
+              <Image src="/bodoni.jpg" className=" point-event-none rounded-3xl z-0" draggable="false" objectFit='cover' fill 	></Image>
+            </div>
+          </motion.div>
+          <div className="w-full aspect-square absolute top-0 z-0">
+            <div className="w-full aspect-square relative">
+              <Image src="/Abstract Blue Carte Da Gioco Texture.jpg" className="point-event-none rounded-3xl " draggable="false" objectFit='cover' fill 	></Image>
+            </div>
+          </div>
+        </div>
+        {/**text */}
+        <div className="w-full flex flex-col text-center">
+          <h1>Gian Battista Bodoni</h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pharetra arcu nec bibendum tincidunt. Nullam dictum purus in magna molestie blandit. Vivamus imperdiet urna justo, a egestas erat dictum sit amet. Sed sodales metus in neque pretium, ac imperdiet metus viverra. Nulla malesuada id justo sit amet molestie. Ut non eleifend neque. Curabitur eget lacinia dolor, ut consequat orci. Maecenas molestie ac leo quis aliquet. Cras lacinia vitae nulla vel pharetra.</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
