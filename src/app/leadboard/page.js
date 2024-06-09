1b20381899722754cd6225cc400fa9a950a03f02
pippoide <giovannipoma28@gmail.com>
2024-06-09 02:56:53 +0200

    update css

--- src/app/leadboard/page.js
+++ src/app/leadboard/page.js
@@ -17,24 +17,34 @@ export default function Leadboard() {
 
 
     return (
-        <div className="w-full h-screen bg-third flex justify-center py-12">
-            <div className="w-full md:w-2/5 bg-secondary text-third ">
-                <div className="grid grid-cols-9 text-secondary font-alfa bg-red-500 bg-primary p-2 md:py-4 text-xs md:text-xl">
-                    <div className="col-span-2">Rank</div>
-                    <div className="col-span-5 text-center">Nickname</div>
-                    <div className="col-span-2 text-right">Score</div>
+        <div className="w-full h-screen bg-third flex justify-center p-12 md:px-0">
+            <div className="w-full md:w-2/5 ">
+                <div className="mb-6 md:mb-12 text-secondary ">
+                    <h1 className="font-alfa text-secondary md:text-6xl ">Classifica</h1>
+                    <p className="text-secondary text-xs md:text-lg">
+                        Qui sono presenti le geste eroiche ti tutti i giocatori che sono riusciti a lasciare un segno nella storia della grafica
+                        </p>
                 </div>
-                <div className="grid grid-cols-1 w-full max-h-[50vh] overflow-y-auto ">
-                    {rows.map((rows, index) => {
-                        return (
-                            <div className="grid grid-cols-9 border-b-2 p-2   " key={index}>
-                                <h1 className="col-span-2 py-1 ">{index + 1}th</h1>
-                                <h1 className="col-span-5 py-1 text-center ">{rows.name}</h1>
-                                <h1 className="col-span-2 py-1 text-right ">{rows.score}</h1>
 
-                            </div>)
-                    })}
+                <div className=" bg-secondary text-third">
+                    <div className="grid grid-cols-9 text-secondary font-alfa bg-red-500 bg-primary p-2 md:py-4 text-xs md:text-xl">
+                        <div className="col-span-2">Rank</div>
+                        <div className="col-span-5 text-center">Nickname</div>
+                        <div className="col-span-2 text-right">Score</div>
+                    </div>
+                    <div className="grid grid-cols-1 w-full max-h-[50vh] overflow-y-auto ">
+                        {rows.map((rows, index) => {
+                            return (
+                                <div className="grid grid-cols-9 border-b-2 p-2 text-xs md:text-lg  " key={index}>
+                                    <h1 className="col-span-2 py-1 ">{index + 1}th</h1>
+                                    <h1 className="col-span-5 py-1 text-center ">{rows.name}</h1>
+                                    <h1 className="col-span-2 py-1 text-right ">{rows.score}</h1>
+
+                                </div>)
+                        })}
+                    </div>
                 </div>
+
 
             </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,9 @@ export default async function Home() {
       <div className="w-full md:w-1/3 flex justify-center flex-col">
         <img className="w-full" src="Logo.svg" alt="logo" />
         <div className="h-3/5 flex flex-col justify-start my-6 md:my-12">
-          <h1 className="text-xs md:text-lg w.full text-wrap mb-6 ">Prova a ripercorrere la storia della grafica nella vita di un commesso tipografo</h1>
+          <p className="text-xs md:text-lg w.full text-left mb-6 ">
+          Vai al tuo lavoro di commesso tipografo e fai scorrere le tue dita a sinistra o a destra, rispondendo cortesemente (o maleducato) ai clienti. Modifica l’andamento della storia della grafica e cerca di scoprire la sua fine…
+          </p>
           <div className="grid w-full grid-rows-3 text-center gap-2">
             <Link href="/game" className="bg-red-50 px-4 py-2 bg-primary hover:bg-primary-light hover:text-third transition-all ease-out duration-300">Gioca</Link>
             <Link href="/history" className="bg-red-50 px-4 py-2 bg-primary hover:bg-primary-light hover:text-third transition-all ease-out duration-300">Cronologia</Link>
