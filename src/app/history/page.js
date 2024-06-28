b439dfbf028e268ecb4527494d80c9e6fe9f9c93
pippoide <giovannipoma28@gmail.com>
2024-06-28 02:25:07 +0200

    responsive

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -12,7 +12,7 @@ export default function History(){
     const carta = mazzo.carta[contatoreCarta];
 
     return(
-        <div className="w-full min-h-screen md:h-screen p-12 flex bg-secondary flex-col">
+        <div className="w-full min-h-screen h-full md:h-screen p-12 flex bg-secondary flex-col overflow-auto">
             <div className="w-full flex flex-col font-custom mb-12">
                 <HomeButton/>
                 <h1 className="text-3xl">CRONOLOGIA</h1>
@@ -21,7 +21,7 @@ export default function History(){
             <div className="w-full h-auto md:h-[30%] flex flex-col md:flex-row items-center overflow-auto space-y-3 md:space-y-0 md:space-x-3">
             {mazzo.carta.filter((carta) => carta.important).map((carta,index)=>{
             return (
-            <div key={index}  className="h-[18rem] md:h-full w-min rounded-3xl flex flex-col space-y-2"> 
+            <div key={index}  className="h-[18rem] md:h-full md:max-w-[12rem] w-full rounded-3xl flex flex-col space-y-2"> 
                 <p className="font-custom w-full text-center bg-primary-light text-secondary rounded-full ">{carta.anno}</p>
                 <div className='overflow-auto h-full p-2 flex-col rounded-xl  bg-primary-light '>
                     <img className='point-event-none bg-cover' sizes='100vw, 30vw' src={carta.flipImg}></img>
